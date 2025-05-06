15f95f3a0b0858852d59993895d620388b60aca6
davijuvino <davijuvinosouza@gmail.com>
2025-05-06 00:28:13 -0300

    add novo componente navigation

---
+++ src/app/app-navigation/app-navigation.component.spec.ts
@@ -0,0 +1,31 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppNavigationComponent } from './app-navigation.component';
+
+describe('AppNavigationComponent', () => {
+  let component: AppNavigationComponent;
+  let fixture: ComponentFixture<AppNavigationComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppNavigationComponent],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppNavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavigationComponent } from "./app-navigation/app-navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
