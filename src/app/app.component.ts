286b09197861a991ba21ce4193678380d78acfb0
davijuvino <davijuvinosouza@gmail.com>
2025-05-11 01:07:57 -0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./component/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
