ddfff9c0841af36343c1a3530223d68a0114bf61
davijuvino <davijuvinosouza@gmail.com>
2025-05-17 15:35:09 -0300

    Add refaturar mobile app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./component/navigation/navigation.component";
 
 @Component({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', children: [
+            {   path: '', 
+                loadComponent: () => import(`./component/dashboard/dashboard.component`)
+                    .then(mod => mod.DashboardComponent)
+        },
+            {
+                path: 'about',
+                loadComponent: () => import(`./pages/about/about.component`)
+                    .then(mod => mod.AboutComponent)
+            },
+            {
+                path: '404',
+                loadComponent: () => import(`./pages/error/error.component`)
+                    .then(mod => mod.ErrorComponent)
+            },
+            {
+                path: '**',
+                redirectTo: '404',
+            },
+        ]
+    },
+];
